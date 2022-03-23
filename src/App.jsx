8c8670550b2773cbb391b30f5a11eaf67346aa1d
vitornogueira <vitor_mbn@hotmail.com>
2022-03-23 19:34:02 -0300

    Exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 import "./App.css";
+import React, {useState} from "react";
 
 function App() {
+
+  const [lista, setLista  ] = useState([
+    "Queijo",
+    "Leite",
+    "Pão",
+    "Coca"
+  ]);
+function adicionar(event){
+  event.preventDefault();
+  const valor = document.getElementById("item").value;
+  setLista([...lista, valor]);
+  document.getElementById("item").value="";
+}
   return (
     <div className="App">
       <header>
-        <h2>Lista de Compras:</h2>
+        <h2>Lista de Comprais:</h2>
       </header>
       <div className="lista-compras-container">
         <ul className="lista-items">
-          <li>Queijo</li>
-          <li>Leite</li>
-          <li>Pão</li>
+          {lista.map(item => <li>{item}</li>)}
         </ul>
       </div>
       <form className="form-add-item" action="#" method="post">
         <fieldset>
           <div className="form-group mb-3">
+    
             <label htmlFor="item">Adicionar Novo Item na Lista:</label>
             <input type="text" className="form-control" id="item" />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onClick={adicionar} >
             Adicionar
           </button>
         </fieldset>
